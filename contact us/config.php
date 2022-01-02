d2bd892f9b3b1e7f55bdbfecbddfd56f256ecc61
Jai Kumar Dadhich <84243610+coderRj42@users.noreply.github.com>
2022-01-02 11:58:04 +0530

    Add files via upload

---
+++ contact us/config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+define (DB_USER, "root");
+define (DB_PASSWORD, "root");
+define (DB_DATABASE, "sole");
+define (DB_HOST, "localhost");
+
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+?>
---
+++ contact us/data.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require('config.php');
+
+
+extract($_POST);
+
+
+$sql = "INSERT into contactus (name,email,message,created_date) VALUES('" . $name . "','" . $email . "','" . $message . "','" . date('Y-m-d') . "')";
+
+
+$success = $mysqli->query($sql);
+
+
+if (!$success) {
+    die("Couldn't enter data: ".$mysqli->error);
+}
+
+
+echo "Thank You For Contacting Us ";
+
+
+$conn->close();
